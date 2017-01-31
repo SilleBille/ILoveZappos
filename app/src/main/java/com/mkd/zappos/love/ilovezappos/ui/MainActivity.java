273bf9b070d380bf373263653898b7a2d08a8158
dinesh prasanth <mkdineshprasanth@gmail.com>
2017-01-30 22:34:43 -0500

    bug fix: fixed the bug of changing screen when product is loaded. Added material design for WolfPack!

--- app/src/main/java/com/mkd/zappos/love/ilovezappos/ui/MainActivity.java
+++ app/src/main/java/com/mkd/zappos/love/ilovezappos/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mkd.zappos.love.ilovezappos.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,10 +12,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.mkd.zappos.love.ilovezappos.BuildConfig;
 import com.mkd.zappos.love.ilovezappos.R;
+import com.mkd.zappos.love.ilovezappos.databinding.ActivityMainBinding;
 import com.mkd.zappos.love.ilovezappos.model.data.JSONResponse;
 import com.mkd.zappos.love.ilovezappos.model.data.Product;
 import com.mkd.zappos.love.ilovezappos.model.remote.ZapposAPI;
@@ -31,17 +34,18 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private EditText etSearchBox;
-    private Button btSearch;
+    private ImageButton btSearch;
     ZapposService service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         etSearchBox = (EditText) findViewById(R.id.et_search);
-        btSearch = (Button) findViewById(R.id.bt_search);
+        btSearch = (ImageButton) findViewById(R.id.bt_search);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -55,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
         service.setUpdateListener(new OnProductResult() {
             @Override
             public void onResultReceived(List<Product> searchResponse) {
-                Toast.makeText(getApplicationContext(), searchResponse.get(0).getProductName(), Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), searchResponse.get(0).getProductName(), Toast.LENGTH_SHORT).show();
+                binding.mainContent.setProduct(searchResponse.get(0));
             }
         });
 
